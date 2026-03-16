bf446491d2d0ed59117c2dbe21f2561f5d59a27b
Hemanjali <kadamatihemanjali25@gmail.com>
2026-03-16 12:32:50 +0530

    Added Redis Cache

--- app/services/url_service.py
+++ app/services/url_service.py
@@ -1,6 +1,7 @@
 from app import db
 from app.models.url_model import URL
 from app.utils.base62 import encode_base62
+from app.utils.redis_client import redis_client
 
 
 def create_short_url(original_url):
@@ -19,11 +20,19 @@ def create_short_url(original_url):
 
 def get_original_url(short_code):
 
+    cached_url = redis_client.get(short_code)
+
+    if cached_url:
+        return cached_url
+
     url = URL.query.filter_by(short_code=short_code).first()
 
     if url:
         url.clicks += 1
         db.session.commit()
+
+        redis_client.set(short_code, url.original_url)
+
         return url.original_url
 
     return None
